198dbf6a8fd0a90b3b97eec95c41f751d2bea80d
ludovico <ludovix9070@gmail.com>
2025-01-03 18:43:09 +0100

    Fixed fusion algo to handle more node types

--- internal/fc_fusion/fusion_dag.go
+++ internal/fc_fusion/fusion_dag.go
@@ -15,17 +15,18 @@ import (
 	"github.com/lithammer/shortuuid"
 )
 
-// Evaluate fusion between nodes in the dag
+/*
+// versione originale funzionante
 func FuseFc(fcomp *fc.FunctionComposition) (bool, error) {
 	PrintDag(&fcomp.Workflow)
 	visited := map[fc.DagNodeId]bool{}
 	nodeQueue := []fc.DagNodeId{fcomp.Workflow.Start.GetNext()[0]}
 	changed := false
 
-	/*err := cleanCurrentComp(fcomp)
-	if err != nil {
-		return false, err
-	}*/
+	//err := cleanCurrentComp(fcomp)
+	//if err != nil {
+	//return false, err
+	//}
 
 	for len(nodeQueue) > 0 {
 		currentId := nodeQueue[0]
@@ -144,8 +145,8 @@ func FuseFc(fcomp *fc.FunctionComposition) (bool, error) {
 							delete(fcomp.Functions, func2.Name)
 						}
 
-						/*// Continue from the current node
-						nodeQueue = append(nodeQueue, mergedNode.GetNext()...)*/
+						// Continue from the current node
+						//nodeQueue = append(nodeQueue, mergedNode.GetNext()...)
 
 						// Re-evaluate the newly merged node for further fusions
 						nodeQueue = append([]fc.DagNodeId{mergedNode.Id}, nodeQueue...)
@@ -171,6 +172,393 @@ func FuseFc(fcomp *fc.FunctionComposition) (bool, error) {
 	fmt.Println("DIM FINALE ", len(fcomp.Workflow.Nodes))
 
 	return true, nil
+}
+*/
+
+type QueueNode struct {
+	NodeID    fc.DagNodeId // ID del nodo nel DAG
+	IsFusible bool         // Se il nodo è stato valutato come fusibile
+	Evaluated bool         //Se il nodo è stato già valutato nei casi choice e fanout
+}
+
+// Evaluate fusion between nodes in the dag
+func FuseFc(fcomp *fc.FunctionComposition) (bool, error) {
+	PrintDag(&fcomp.Workflow)
+	visited := map[fc.DagNodeId]bool{}
+	//nodeQueue := []fc.DagNodeId{fcomp.Workflow.Start.GetNext()[0]}
+	nodeQueue := []QueueNode{{NodeID: fcomp.Workflow.Start.GetNext()[0], IsFusible: false, Evaluated: false}}
+
+	changed := false
+
+	//err := cleanCurrentComp(fcomp)
+	//if err != nil {
+	//return false, err
+	//}
+
+	for len(nodeQueue) > 0 {
+		current := nodeQueue[0]
+		nodeQueue = nodeQueue[1:]
+
+		currentId := current.NodeID
+		fmt.Println("DagNodeId corrente ", currentId)
+
+		if visited[currentId] {
+			continue
+		}
+		visited[currentId] = true
+
+		currentNode, ok := fcomp.Workflow.Find(currentId)
+		if !ok {
+			continue
+		}
+
+		// Check if it's a simple node
+		if simpleNode, ok := currentNode.(*fc.SimpleNode); ok {
+			nextNodes := simpleNode.GetNext()
+			if len(nextNodes) == 1 {
+				nextNodeId := nextNodes[0]
+				nextNode, ok := fcomp.Workflow.Find(nextNodeId)
+				if ok {
+					// Check if next node is a simple node
+					if nextSimpleNode, ok := nextNode.(*fc.SimpleNode); ok {
+						//controlli necessari per valutare se fusione effettivamente ok
+						//posso essere stato già valutato se vengo da choice o fanout
+						//o se sono il next di un simple node candidato alla fusione
+						if !current.Evaluated {
+							current.Evaluated = true
+							if CountNodeReferences(simpleNode.Id, &fcomp.Workflow) > 1 {
+								current.IsFusible = false
+								for _, nextNodeId := range simpleNode.GetNext() {
+									nodeQueue = append(nodeQueue, QueueNode{NodeID: nextNodeId, IsFusible: false, Evaluated: false})
+								}
+								continue
+							} else {
+								current.IsFusible = true
+								if CountNodeReferences(nextSimpleNode.Id, &fcomp.Workflow) > 1 {
+									for _, nextNodeId := range simpleNode.GetNext() {
+										nodeQueue = append(nodeQueue, QueueNode{NodeID: nextNodeId, IsFusible: false, Evaluated: true})
+									}
+									continue
+								}
+							}
+
+						} else if !current.IsFusible {
+							for _, nextNodeId := range simpleNode.GetNext() {
+								nodeQueue = append(nodeQueue, QueueNode{NodeID: nextNodeId, IsFusible: false, Evaluated: false})
+							}
+							continue
+						} else {
+							if CountNodeReferences(nextSimpleNode.Id, &fcomp.Workflow) > 1 {
+								for _, nextNodeId := range simpleNode.GetNext() {
+									nodeQueue = append(nodeQueue, QueueNode{NodeID: nextNodeId, IsFusible: false, Evaluated: true})
+								}
+								continue
+							}
+						}
+
+						// Fusion
+						func1, ok := function.GetFunction(simpleNode.Func)
+						if !ok {
+							return false, fmt.Errorf("fun1 error")
+						}
+						func2, ok := function.GetFunction(nextSimpleNode.Func)
+						if !ok {
+							return false, fmt.Errorf("fun2 error")
+						}
+
+						if containsPython(func1.Runtime) && containsPython(func2.Runtime) {
+							if func1.Runtime != func2.Runtime {
+								for _, nextNodeId := range simpleNode.GetNext() {
+									nodeQueue = append(nodeQueue, QueueNode{NodeID: nextNodeId, IsFusible: false, Evaluated: false})
+								}
+								continue
+							}
+						} else {
+							for _, nextNodeId := range simpleNode.GetNext() {
+								nodeQueue = append(nodeQueue, QueueNode{NodeID: nextNodeId, IsFusible: false, Evaluated: false})
+							}
+							continue
+						}
+
+						mergedFunc, error := CombineFunctions(func1, func2)
+						fmt.Println("Primo giro")
+						if error != nil {
+							fmt.Println("Giro con errore ", error)
+							return false, fmt.Errorf("combining functions error")
+						}
+
+						//Update Functions structure of the composition with the new fused func
+						fcomp.Functions[mergedFunc.Name] = mergedFunc
+						//per evitare esplosioni, si può usare la versione usata per un normale simple node invece che per uno proveniente da asl
+						//newNodeId := fc.DagNodeId(fmt.Sprintf("%s_%s", simpleNode.Id, nextSimpleNode.Id))
+						newNodeId := fc.DagNodeId(shortuuid.New())
+
+						mergedNode := &fc.SimpleNode{
+							Id:       newNodeId,
+							NodeType: simpleNode.NodeType,
+							BranchId: simpleNode.BranchId,
+							OutputTo: nextSimpleNode.OutputTo,
+							Func:     mergedFunc.Name,
+						}
+
+						// Adding new node to the dag
+						fcomp.Workflow.Nodes[newNodeId] = mergedNode
+
+						//qui aggiorno i riferimenti di tutti i nodi che riferiscono il primo nodo
+						for _, prevNode := range fcomp.Workflow.Nodes {
+							switch node := prevNode.(type) {
+							case *fc.StartNode:
+								//if node.Next == simpleNode.Id || node.Next == nextSimpleNode.Id {
+								if node.Next == simpleNode.Id {
+									//node.Next = newNodeId
+									node.SetNext(newNodeId)
+									fcomp.Workflow.Start = node
+								}
+
+							case *fc.SimpleNode:
+								//if node.OutputTo == simpleNode.Id || node.OutputTo == nextSimpleNode.Id {
+								if node.OutputTo == simpleNode.Id {
+									node.OutputTo = newNodeId
+								}
+
+							case *fc.FanOutNode:
+								for i, next := range node.OutputTo {
+									//if next == simpleNode.Id || next == nextSimpleNode.Id {
+									if next == simpleNode.Id {
+										node.OutputTo[i] = newNodeId
+									}
+								}
+
+							case *fc.FanInNode:
+								//if node.OutputTo == simpleNode.Id || node.OutputTo == nextSimpleNode.Id {
+								if node.OutputTo == simpleNode.Id {
+									node.OutputTo = newNodeId
+								}
+
+							case *fc.ChoiceNode:
+								for i, next := range node.Alternatives {
+									//if next == simpleNode.Id || next == nextSimpleNode.Id {
+									if next == simpleNode.Id {
+										node.Alternatives[i] = newNodeId
+									}
+								}
+
+							default:
+								log.Printf("Unhandled node type: %T", node)
+							}
+						}
+
+						// Remove old nodes
+						delete(fcomp.Workflow.Nodes, simpleNode.Id)
+						delete(fcomp.Workflow.Nodes, nextSimpleNode.Id)
+
+						// Removing functions use by fused node if not used in another node in the dag
+						if !IsFunctionUsed(func1.Name, &fcomp.Workflow) {
+							delete(fcomp.Functions, func1.Name)
+						}
+						if !IsFunctionUsed(func2.Name, &fcomp.Workflow) {
+							delete(fcomp.Functions, func2.Name)
+						}
+
+						// Continue from the current node
+						//nodeQueue = append(nodeQueue, mergedNode.GetNext()...)
+
+						// Re-evaluate the newly merged node for further fusions
+						nodeQueue = append(nodeQueue, QueueNode{NodeID: mergedNode.Id, IsFusible: false, Evaluated: false})
+
+						changed = true
+						continue
+					}
+				}
+			}
+
+			//Add next node to visit queue
+			for _, nextNodeId := range simpleNode.GetNext() {
+				nodeQueue = append(nodeQueue, QueueNode{NodeID: nextNodeId, IsFusible: false, Evaluated: false})
+			}
+		} else {
+			switch node := currentNode.(type) {
+			case *fc.ChoiceNode:
+				// Aggiungi tutte le alternative alla coda
+				for _, alt := range node.Alternatives {
+					nextNode, ok := fcomp.Workflow.Find(alt)
+					if ok {
+						if nextSimpleNode, ok := nextNode.(*fc.SimpleNode); ok {
+							fusible := CanFuseChoiceNode(nextSimpleNode, &fcomp.Workflow, node)
+							nodeQueue = append(nodeQueue, QueueNode{NodeID: alt, IsFusible: fusible, Evaluated: true})
+						} else {
+							//non è un simple node, lo valuto di default come non fusible
+							nodeQueue = append(nodeQueue, QueueNode{NodeID: alt, IsFusible: false, Evaluated: true})
+						}
+					}
+				}
+
+			case *fc.FanOutNode:
+				for _, branch := range node.OutputTo {
+					nextNode, ok := fcomp.Workflow.Find(branch)
+					if ok {
+						if nextSimpleNode, ok := nextNode.(*fc.SimpleNode); ok {
+							fusible := CanFuseFanOutNode(nextSimpleNode, &fcomp.Workflow, node)
+							nodeQueue = append(nodeQueue, QueueNode{NodeID: branch, IsFusible: fusible, Evaluated: true})
+						} else {
+							nodeQueue = append(nodeQueue, QueueNode{NodeID: branch, IsFusible: false, Evaluated: true})
+						}
+					}
+				}
+
+			case *fc.FanInNode:
+				// Aggiungi il nodo successivo alla coda
+				if node.OutputTo != "" {
+					nodeQueue = append(nodeQueue, QueueNode{NodeID: node.OutputTo, IsFusible: false, Evaluated: false})
+				}
+
+			default:
+				log.Printf("Unhandled node type: %T", node)
+			}
+		}
+	}
+
+	if changed {
+		err := replaceWithNewFc(fcomp)
+		if err != nil {
+			return false, err
+		}
+	}
+
+	fmt.Println("DIM FINALE ", len(fcomp.Workflow.Nodes))
+
+	return true, nil
+}
+
+// se sono simple node e sono next di un choice node, per essere fusibile devo essere referenziato una sola volta
+// dal choice node e stop altrimenti non posso essere fuso
+// controllo se sono referenziato da altri choice node o da altri nodi in generale nel dag
+func CanFuseChoiceNode(node *fc.SimpleNode, dag *fc.Dag, choiceNode *fc.ChoiceNode) bool {
+	// Verifica che il nodo non sia referenziato in altri rami dello stesso ChoiceNode
+	/*//fa un controllo sulla funzione di un nodo non sul fatto che un nodo venga riferito oppure no
+	for _, alt := range choiceNode.Alternatives {
+		if alt == node.Id {
+			continue
+		}
+
+		otherNode, ok := dag.Nodes[alt]
+		if !ok {
+			continue
+		}
+
+		if simpleNode, ok := otherNode.(*fc.SimpleNode); ok {
+			if simpleNode.Func == node.Func {
+				return false // Il nodo è condiviso in altri rami dello stesso ChoiceNode
+			}
+		}
+	}*/
+
+	// Verifica che il nodo non sia referenziato da altri ChoiceNode nel DAG
+	for _, otherNode := range dag.Nodes {
+		if otherChoiceNode, ok := otherNode.(*fc.ChoiceNode); ok && otherChoiceNode != choiceNode {
+			for _, alt := range otherChoiceNode.Alternatives {
+				if alt == node.Id {
+					return false // Nodo referenziato da un altro ChoiceNode
+				}
+			}
+		}
+	}
+
+	// Verifica che il nodo non sia referenziato da altri nodi generici nel DAG
+	for _, otherNode := range dag.Nodes {
+		if otherNode == choiceNode {
+			continue
+		}
+		if hasNext, ok := otherNode.(fc.HasNext); ok {
+			for _, next := range hasNext.GetNext() {
+				if next == node.Id {
+					return false // Nodo referenziato da un altro nodo
+				}
+			}
+		}
+	}
+
+	return true
+}
+
+func CanFuseFanOutNode(node *fc.SimpleNode, dag *fc.Dag, fanOutNode *fc.FanOutNode) bool {
+	// Verifica che il nodo non sia referenziato in altri rami dello stesso FanOutNode
+	//check solo se funzione è uguale
+	/*for _, branch := range fanOutNode.OutputTo {
+		if branch == node.Id {
+			continue
+		}
+
+		otherNode, ok := dag.Nodes[branch]
+		if !ok {
+			continue
+		}
+
+		if simpleNode, ok := otherNode.(*fc.SimpleNode); ok {
+			if simpleNode.Func == node.Func {
+				return false // Il nodo è condiviso in altri rami dello stesso FanOutNode
+			}
+		}
+	}*/
+
+	// Verifica che il nodo non sia referenziato da altri nodi generici nel DAG
+	if IsNodeReferenced(node.Id, dag) {
+		return false
+	}
+
+	return true
+}
+
+func IsNodeReferenced(nodeId fc.DagNodeId, dag *fc.Dag) bool {
+	for _, otherNode := range dag.Nodes {
+		// Verifica se il nodo è presente nella lista 'Next' di un nodo che implementa 'HasNext'
+		if hasNext, ok := otherNode.(fc.HasNext); ok {
+			for _, next := range hasNext.GetNext() {
+				if next == nodeId {
+					return true // Nodo referenziato
+				}
+			}
+		}
+
+		// Verifica se il nodo è presente nella lista di Alternative di un ChoiceNode
+		if choiceNode, ok := otherNode.(*fc.ChoiceNode); ok {
+			for _, alt := range choiceNode.Alternatives {
+				if alt == nodeId {
+					return true // Nodo referenziato in un ChoiceNode
+				}
+			}
+		}
+	}
+	return false // Nodo non referenziato
+}
+
+// count references to a simple node, to be fused must be only one
+func CountNodeReferences(nodeId fc.DagNodeId, dag *fc.Dag) int {
+	count := 0
+
+	for _, otherNode := range dag.Nodes {
+		// Verifica se il nodo è presente nella lista 'Next' di un nodo che implementa 'HasNext'
+		if hasNext, ok := otherNode.(fc.HasNext); ok {
+			for _, next := range hasNext.GetNext() {
+				if next == nodeId {
+					count++ // Nodo referenziato
+				}
+			}
+		}
+
+		// Verifica se il nodo è presente nella lista di Alternative di un ChoiceNode
+		if choiceNode, ok := otherNode.(*fc.ChoiceNode); ok {
+			for _, alt := range choiceNode.Alternatives {
+				if alt == nodeId {
+					count++ // Nodo referenziato in un ChoiceNode
+				}
+			}
+		}
+	}
+
+	fmt.Println("dag node id ", nodeId)
+	fmt.Println("counter ", count)
+	return count // Nodo non referenziato
 }
 
 func IsFunctionUsed(funcName string, dag *fc.Dag) bool {
